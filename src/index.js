4830b8b7c56e0b86edb998a9435e82dd85fb978f
Shubham Joshi <sjoshi91119@gmail.com>
2023-07-31 11:07:08 +0530

    done with it

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"; // Import your main component that contains the Router
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers/rootReducer";
-
-const store = configureStore({
-  reducer: rootReducer,
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
